1bdae7d9480c663c41a80e739d3505b278dab700
rabdumalikov <vigosslive2@gmail.com>
2021-12-24 14:24:00 +0200

    incident

---
+++ build/release/catchtest-download/CMakeFiles/catchtest-download.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/catchtest-download"
+  "CMakeFiles/catchtest-download-complete"
+  "catchtest-download-prefix/src/catchtest-download-stamp/catchtest-download-build"
+  "catchtest-download-prefix/src/catchtest-download-stamp/catchtest-download-configure"
+  "catchtest-download-prefix/src/catchtest-download-stamp/catchtest-download-download"
+  "catchtest-download-prefix/src/catchtest-download-stamp/catchtest-download-install"
+  "catchtest-download-prefix/src/catchtest-download-stamp/catchtest-download-mkdir"
+  "catchtest-download-prefix/src/catchtest-download-stamp/catchtest-download-patch"
+  "catchtest-download-prefix/src/catchtest-download-stamp/catchtest-download-skip-update"
+  "catchtest-download-prefix/src/catchtest-download-stamp/catchtest-download-test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/catchtest-download.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/release/googlebench-build/src/CMakeFiles/benchmark.dir/DependInfo.cmake
@@ -0,0 +1,48 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/v/Desktop/value/build/release/googlebench-src/src/benchmark.cc" "/home/v/Desktop/value/build/release/googlebench-build/src/CMakeFiles/benchmark.dir/benchmark.cc.o"
+  "/home/v/Desktop/value/build/release/googlebench-src/src/benchmark_api_internal.cc" "/home/v/Desktop/value/build/release/googlebench-build/src/CMakeFiles/benchmark.dir/benchmark_api_internal.cc.o"
+  "/home/v/Desktop/value/build/release/googlebench-src/src/benchmark_name.cc" "/home/v/Desktop/value/build/release/googlebench-build/src/CMakeFiles/benchmark.dir/benchmark_name.cc.o"
+  "/home/v/Desktop/value/build/release/googlebench-src/src/benchmark_register.cc" "/home/v/Desktop/value/build/release/googlebench-build/src/CMakeFiles/benchmark.dir/benchmark_register.cc.o"
+  "/home/v/Desktop/value/build/release/googlebench-src/src/benchmark_runner.cc" "/home/v/Desktop/value/build/release/googlebench-build/src/CMakeFiles/benchmark.dir/benchmark_runner.cc.o"
+  "/home/v/Desktop/value/build/release/googlebench-src/src/colorprint.cc" "/home/v/Desktop/value/build/release/googlebench-build/src/CMakeFiles/benchmark.dir/colorprint.cc.o"
+  "/home/v/Desktop/value/build/release/googlebench-src/src/commandlineflags.cc" "/home/v/Desktop/value/build/release/googlebench-build/src/CMakeFiles/benchmark.dir/commandlineflags.cc.o"
+  "/home/v/Desktop/value/build/release/googlebench-src/src/complexity.cc" "/home/v/Desktop/value/build/release/googlebench-build/src/CMakeFiles/benchmark.dir/complexity.cc.o"
+  "/home/v/Desktop/value/build/release/googlebench-src/src/console_reporter.cc" "/home/v/Desktop/value/build/release/googlebench-build/src/CMakeFiles/benchmark.dir/console_reporter.cc.o"
+  "/home/v/Desktop/value/build/release/googlebench-src/src/counter.cc" "/home/v/Desktop/value/build/release/googlebench-build/src/CMakeFiles/benchmark.dir/counter.cc.o"
+  "/home/v/Desktop/value/build/release/googlebench-src/src/csv_reporter.cc" "/home/v/Desktop/value/build/release/googlebench-build/src/CMakeFiles/benchmark.dir/csv_reporter.cc.o"
+  "/home/v/Desktop/value/build/release/googlebench-src/src/json_reporter.cc" "/home/v/Desktop/value/build/release/googlebench-build/src/CMakeFiles/benchmark.dir/json_reporter.cc.o"
+  "/home/v/Desktop/value/build/release/googlebench-src/src/perf_counters.cc" "/home/v/Desktop/value/build/release/googlebench-build/src/CMakeFiles/benchmark.dir/perf_counters.cc.o"
+  "/home/v/Desktop/value/build/release/googlebench-src/src/reporter.cc" "/home/v/Desktop/value/build/release/googlebench-build/src/CMakeFiles/benchmark.dir/reporter.cc.o"
+  "/home/v/Desktop/value/build/release/googlebench-src/src/sleep.cc" "/home/v/Desktop/value/build/release/googlebench-build/src/CMakeFiles/benchmark.dir/sleep.cc.o"
+  "/home/v/Desktop/value/build/release/googlebench-src/src/statistics.cc" "/home/v/Desktop/value/build/release/googlebench-build/src/CMakeFiles/benchmark.dir/statistics.cc.o"
+  "/home/v/Desktop/value/build/release/googlebench-src/src/string_util.cc" "/home/v/Desktop/value/build/release/googlebench-build/src/CMakeFiles/benchmark.dir/string_util.cc.o"
+  "/home/v/Desktop/value/build/release/googlebench-src/src/sysinfo.cc" "/home/v/Desktop/value/build/release/googlebench-build/src/CMakeFiles/benchmark.dir/sysinfo.cc.o"
+  "/home/v/Desktop/value/build/release/googlebench-src/src/timers.cc" "/home/v/Desktop/value/build/release/googlebench-build/src/CMakeFiles/benchmark.dir/timers.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "HAVE_POSIX_REGEX"
+  "HAVE_STD_REGEX"
+  "HAVE_STEADY_CLOCK"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "googlebench-src/include"
+  "googlebench-src/src"
+  "googlebench-src/src/../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
