6ca991e8e68e4f77ff6775ff647fe45f6bb93717
rabdumalikov <vigosslive2@gmail.com>
2021-12-24 14:09:50 +0200

    initial working version

---
+++ benchmarks/qs_benchmarks.cpp
@@ -0,0 +1,15 @@
+//  Copyright (c) 2019 Rustam Abdumalikov
+//
+//  "eswitch_v5" library
+//
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+
+#undef NDEBUG
+#include <cassert>
+
+#include "benchmark/benchmark.h"
+
+BENCHMARK_MAIN();
