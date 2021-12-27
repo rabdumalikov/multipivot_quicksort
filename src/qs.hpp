952ae566d9910507e9a00b266f1bc0952db9838e
Aral Acikalin <aralacikalin@hotmail.com>
2021-12-27 14:44:38 +0300

    added greater function to header

--- src/qs.hpp
+++ src/qs.hpp
@@ -1,5 +1,6 @@
 
 #pragma once
+#include <stdint.h>
 
 #include <vector>
 
@@ -7,5 +8,6 @@ std::vector<int64_t> sort_pivots( std::vector<int64_t> & arr, int64_t start, int
 std::vector< int64_t > get_pivot( std::vector< int64_t > & arr, int64_t l, int64_t r, int64_t n );
 std::vector< int64_t > less_or_equal( const std::vector< int64_t > & arr, const std::vector<int64_t> & pivots, const int64_t value );
 std::vector<int64_t> general_partition( std::vector<int64_t> & arr, int64_t l, int64_t r, std::vector<int64_t> & pivots );
+std::vector< int64_t > greater( const std::vector< int64_t > & arr, const std::vector<int64_t> & pivots, const int64_t value );
 
 void quicksort( std::vector< int64_t > & arr, int64_t num_pivots );
